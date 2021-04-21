7fe9fcfc0825ebb04384ffb4be258e37e12e15e0
omarshivji <78180819+omarshivji@users.noreply.github.com>
2021-04-21 21:00:27 +0100

    Initialize feedback

---
+++ AlgorithmSimulator/CHPerfTimer.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading;
+
+namespace AlgorithmSimulator
+{
+    #region High Perfomance Timer
+    /// <summary>
+    /// A timer which works the same way as a stopwatch, 
+    /// you will need to start and stop the timer and then
+    /// get the duration that was elapsed between start and stop.
+    /// </summary>
+    public class CHPerfTimer
+    {
+        private long startTime, stopTime;
+        private long freq;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CHPerfTimer()
+        {
+            startTime = 0;
+            stopTime = 0;
+
+            freq = System.Diagnostics.Stopwatch.Frequency;
+            System.Diagnostics.Stopwatch.StartNew();
+        }
+
+        /// <summary>
+        /// Starts the stopwatch
+        /// </summary>
+        public void Start()
+        {
+            Thread.Sleep(0);
+            startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+        }
+
+        /// <summary>
+        /// Stops the stopwatch
+        /// </summary>
+        public void Stop()
+        {
+            stopTime = System.Diagnostics.Stopwatch.GetTimestamp();
+        }
+
+        /// <summary>
+        /// Gets the duration counted up to now by the stopwatch
+        /// </summary>
+        /// <returns>Returns the duration in seconds</returns>
+        public double GetTimePassed()
+        {
+            return (double)(((double)System.Diagnostics.Stopwatch.GetTimestamp() - (double)startTime) / (double)freq);
+        }
+
+        /// <summary>
+        /// Gets the duration in seconds counted by the stopwatch.
+        /// </summary>
+        /// <returns>Returns the duration in seconds from start to stop.</returns>
+        public double Duration
+        {
+            get
+            {
+                return (double)(((double)stopTime - (double)startTime) / (double)freq);
+            }
+        }
+    }
+    #endregion
+}
---
+++ AlgorithmSimulator/InitialConditions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AlgorithmSimulator
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Creates a 20 integer random array
+        /// </summary>
+        /// <returns>Returns a 20 integer random array</returns>
+        int[] GetRandomIntArray()
+        {
+            int[] randomarray = new int[20];
+            Random _random = new Random();
+
+            for (int i = 0; i < randomarray.Length; i++)
+            {
+                randomarray[i] = _random.Next(0, 500);
+            }
+            return randomarray;
+        }
+
+        /// <summary>
+        /// Creates a 20 integer nearly sorted array. By nearly sorted it means
+        /// that maximum 7 elements in the array are not sorted correctly.
+        /// </summary>
+        /// <returns>Returns a 20 integer nearly sorted array</returns>
+        int[] GetNearlySortedIntArray()
+        {
+            List<int> randomarray = new List<int>();
+            Random _random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                randomarray.Add(_random.Next(0, 500));
+            }
+            randomarray.Sort();
+            for (int i = 0; i < 8; i++)
+            {
+                randomarray[_random.Next(0,19)] = _random.Next(0, 500);
+            }
+            return randomarray.ToArray();
+        }
+
+        /// <summary>
+        /// Creates a 20 integer reversed sorted array
+        /// </summary>
+        /// <returns>Returns a 20 integer reversed sorted array</returns>
+        int[] GetReversedSortedIntArray()
+        {
+            List<int> randomarray = new List<int>();
+            Random _random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                randomarray.Add(_random.Next(0, 500));
+            }
+            randomarray.Sort();
+            randomarray.Reverse();
+            return randomarray.ToArray();
+        }
+    }
+}
